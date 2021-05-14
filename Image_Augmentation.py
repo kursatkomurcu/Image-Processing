6011b4e36fb2e29f475e2e57be84497aa3872121
kursatkomurcu <58462845+kursatkomurcu@users.noreply.github.com>
2021-05-14 23:13:22 +0300

    Update Image_Augmentation.py

--- Image_Augmentation.py
+++ Image_Augmentation.py
@@ -8,7 +8,7 @@ datagen = ImageDataGenerator(rotation_range=40,
                              zoom_range=0.2,
                              horizontal_flip=True,
                              fill_mode='nearest')
-directory = os.chdir('E:\\NutzenTech\\Dataset\\Elektrik Direkleri\\insulator_defective\\images')
+directory = os.chdir('DIRECTORY_PATH')
 
 for i in os.listdir(directory):
     img = load_img(i)
@@ -16,7 +16,7 @@ for i in os.listdir(directory):
     x = x.reshape((1,)+x.shape)
     j = 0
     for batch in datagen.flow(x, batch_size=1,
-                              save_to_dir='E:\\NutzenTech\\Dataset\\Elektrik Direkleri\\insulator_defective\\augmentation',
+                              save_to_dir='SAVE_PATH',
                               save_format='jpg'):
         j +=1
         if j > 10:
